fbe7654c05e884a8c29b027a0f44f6d7ecc27b53
Shwetang <shwetang.mishra@mykukun.com>
2019-11-06 18:22:49 +0530

    implemented heapify for min heap

--- PriorityQueue/heap.py
+++ PriorityQueue/heap.py
@@ -2,17 +2,41 @@ class Heap:
     '''
     Implementation of Heap ADT
     '''
-    def __init__(self):
-        self.heap = []
 
-    def heapify(self,parent_node_to_heapify_from):
+    def __init__(self, heap_type="min", heap=None):
+        if heap is None:
+            self.heap = []
+        else:
+            self.heap = heap
+        self.heap_type = heap_type
+
+    def heapify(self, parent_node_index_to_heapify_from):
         """
         Top-down heapify
         :return:
         """
-        pass
+        if self.heap_type == "min":
+            if len(self.heap) == 1:
+                return
+            parent_index = parent_node_index_to_heapify_from
+            while 2 * parent_index < (len(self.heap) - 2):
+                left_child = 2 * parent_index + 1
+                right_child = 2 * parent_index + 2
+                if self.heap[parent_index] >= self.heap[left_child]:
+                    child_to_swap_with = left_child
+                elif self.heap[parent_index] >= self.heap[right_child]:
+                    child_to_swap_with = right_child
+                else:
+                    return
+                self.heap[parent_index], self.heap[child_to_swap_with] = self.heap[child_to_swap_with], self.heap[
+                    parent_index]
+                parent_index = child_to_swap_with
 
-    def inverse_heapify(self,child_node_to_inverse_heapify_from):
+        else:
+            # TODO: Implement for max heap!
+            pass
+
+    def inverse_heapify(self, child_node_to_inverse_heapify_from):
         """
         Bottom-up heapify
         :return:
@@ -39,14 +63,14 @@ class Heap:
         """
         pass
 
-    def add(self,elem):
+    def add(self, elem):
         """
         Adds element elem to the heap
         :param element:
         :return:
         """
         self.heap.append(elem)
-        self.inverse_heapify(len(self.heap)-1)
+        self.inverse_heapify(len(self.heap) - 1)
 
     def heap_sort(self):
         pass
@@ -55,4 +79,30 @@ class Heap:
         pass
 
     def __str__(self):
-        pass
+        return 'Heap : {}'.format(self.heap)
+
+
+if __name__ == "__main__":
+    h = Heap(heap=[0, 2, 3, 4, 5, 6, 4])
+    # h.add(4)
+    # print(h)
+    # h.add(5)
+    # print(h)
+    # h.add(6)
+    # print(h)
+    # h.add(4)
+    # print(h)
+    # h.add(2)
+    # print(h)
+    # h.add(0)
+    # print(h)
+    # h.add(3)
+    print(h)
+    print(h.poll())
+    print(h)
+    print(h.poll())
+    print(h)
+    print(h.poll())
+    print(h)
+    print(h.poll())
+    print(h)
