b475bceec4a7d981dd018c1c7153a141cf623c1f
Shwetang <shwetang.mishra@mykukun.com>
2019-11-06 15:56:00 +0530

    added skeleton for binary heap

---
+++ PriorityQueue/heap.py
@@ -0,0 +1,42 @@
+class Heap:
+    '''
+    Implementation of Heap ADT
+    '''
+    def __init__(self):
+        self.heap = []
+
+    def heapify(self):
+        """
+        Bottom up heapify
+        :return:
+        """
+        pass
+
+    def inverse_heapify(self):
+        """
+        Top down heapify
+        :return:
+        """
+        pass
+
+    def poll(self):
+        """
+        Remove min element from heap
+        :return:
+        """
+        pass
+
+    def find_and_remove(self):
+        """
+        Finds and removes an element
+        :return:
+        """
+        pass
+
+    def add(self,elem):
+        """
+        Adds element elem to the heap
+        :param element:
+        :return:
+        """
+        pass
